b0f1afaacef243cb7f23ccc1ec6a7069b4ba7ab2
Stephanie Asmar <sasmar8@gmail.com>
2017-08-29 18:36:22 -0400

    adds more notes on linked lists

--- Week-4/LinkedLists.py
+++ Week-4/LinkedLists.py
@@ -12,3 +12,17 @@ a = [1, 2, 3, 4, 5]
 
 for i in range(len(a)):
     print hex(id(a[i]))
+
+
+
+# LINKED LISTS: 
+# Node = value, next
+
+# linked lists do not have to be next to eachother
+# tail value is empty until the next value is added
+# removing from a linked list: point the next element to a different node in linked list
+# no indeces, we never have to shift back in the array
+# How do we search through a linked list? its more like a scavenger hunt...
+# only the previous number knows where the next number is...
+# you have to loop through the entire array to access each number
+# inserting and deleting into a linked list happens in constant time
