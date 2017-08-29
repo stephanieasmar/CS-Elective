7023c509677eb84bead1f4ceb4a27e6cc4865ab6
Stephanie Asmar <sasmar8@gmail.com>
2017-08-29 18:07:48 -0400

    Starts CS elective, week-4 file

---
+++ Week-4/Binary.py
@@ -0,0 +1,24 @@
+# convert base 2 numbers to base 10
+1 = 1
+11 = 3
+111 = 7
+101 = 5
+10101 = 1+0+4+0+16 = 21
+
+1 = 1
+2 = 10
+7 = 111
+11 = 1011
+64 = 1000000
+63 = 111111
+128 = 10000000
+129 = 10000001
+
+# How is memory structured?
+# Hard drive vs RAM (random access memory)
+# Bit is either a 0 or a 1
+# Byte = 8 bits
+# maximum that 8 bytes can stroe = 255
+
+# integer overflow = computer cant stroe more than 4B bytes, overflow resets memory to zero
+
